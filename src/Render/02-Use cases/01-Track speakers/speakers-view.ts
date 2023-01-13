b9840d42563c0a294057f315a8157bb4276d2db6
Warwick McNaughton <warwick.mcnaughton@gmail.com>
2023-01-14 09:11:27 +1300

    Add info buttons to setup sheet

--- src/Render/02-Use cases/01-Track speakers/speakers-view.ts
+++ src/Render/02-Use cases/01-Track speakers/speakers-view.ts
@@ -102,14 +102,23 @@ const speaker_tracker = `
 </div>
 <!-- Meeting setup slide-out sheet -->
 <div id='mtgsetup-sheet'></div>
-
+<!-- Meeting setup slide-out sheet: setup info -->
+<div id='mtgsetup-info-display' class='sheet-info'></div>
+<!-- Meeting setup slide-out sheet: timer type info -->
+<div id='mtgsetup-info-timer-display' class='sheet-info'></div>
+<!-- Meeting setup slide-out sheet: record info -->
+<div id='mtgsetup-info-record-display' class='sheet-info'></div>
 `
 
 /** The html for the slide-out meeting setup sheet. */  
 const setupMeetingSheet = `
   <div id='mtgsetup-container'>
     <div id='mtgsetup-heading'><h2>Meeting setup</h2></div>
-    <div id='mtgsetup-done'><button id='mtgsetup-done-btn'>Done</button></div>
+    <div id='mtgsetup-info-done'>
+      <button id='mtgsetup-info-btn' class='info-btn'>d</button>
+      <button id='mtgsetup-done-btn'>Done</button>
+    </div>
+  
     <div id='mtgsetup-entity'>
       <div id='mtgsetup-entity-dropdown-container'>
       <label for="mtgsetup-select-entity">Choose an entity:</label>
@@ -122,12 +131,18 @@ const setupMeetingSheet = `
         <select name="mtgsetup-select-group" class="mtgsetup-select" id="mtgsetup-select-group"></select>
       </div>
     </div>
-    <div id='mtgsetup-times' class='switch_container'>
+    <div id='mtgsetup-info-timer'>
+      <button id='mtgsetup-info-timer-btn' class='info-btn'>d</button>
+    </div>
+    <div id='mtgsetup-timer' class='switch_container'>
       <p class='switch_text'>Show individual member times</p>
       <label class="switch">
         <input type="checkbox" id="timer_type">
         <span class="slider"></span>
       </label>
+    </div>
+    <div id='mtgsetup-info-record'>
+      <button id='mtgsetup-info-record-btn' class='info-btn'>d</button>
     </div>
     <div id='mtgsetup-record' class='switch_container'>
       <p class='switch_text'>Create a record of speaking times for this meeting</p>
@@ -138,6 +153,63 @@ const setupMeetingSheet = `
     </div>
   </div>
 `
+/** Html for meeting setup sheet info */
+const mtgSetupInfo = `
+  <div id='mtgSetUpInfoContainer' class='info-container'>
+    <p>
+    Display the members of a meeting group \
+    by selecting the entity and the meeting group.
+    </p>
+    <p>
+    To create an entity, its members and \
+    its meeting groups, go to the 'Setup' tab.
+    </p>
+  </div>
+`
+/** Html for meeting setup sheet timer-type info */
+const mtgSetupInfoTimer = `
+  <div id='mtgSetUpTimerInfoContainer' class='info-container'>
+    <p>
+    After setting an Entity and Meeting group, \
+    turn on 'Show individual member times' to \
+    display each member's time alongside the member's \
+    name.
+    </p>
+    <p>
+    Use the Play, Pause and Stop controls alongside \
+    the member's name to control the member's speaking time.
+    </p>
+  </div>
+`
+/** Html for meeting setup sheet recording info */
+const mtgSetupInfoRecord = `
+  <div id='mtgSetUpRecordInfoContainer' class='info-container'>
+    <p>
+    'Create a record of speaking times for this meeting' \
+    saves speaking times to the chosen meeting event \
+    (meeting events are created separately in Setup).
+    </p>
+    <p>
+    The Entity and Meeting group must have been selected \
+    and 'Show individual member times' must be turned on .
+    </p>
+    <p>
+    Press 'Save debate'to save the current debate and \
+    create a new one.
+    </p>
+    <p>
+    Press 'End this meeting' to complete the report \
+    and reset.
+    </p>
+    <p>
+    View the report in 'Reports'.
+    </p>
+    <p>
+    To create a meeting event, go to the Setup tab then select Events.
+    </p>
+  </div>
+`
+
 
 /** 
  * Loads the meeting setup html into the DOM. Adds listeners for 
@@ -146,10 +218,47 @@ const setupMeetingSheet = `
 const loadSetupMeetingSheet = () => {
   const mtgsht = document.getElementById('mtgsetup-sheet')
   if (mtgsht) {mtgsht.innerHTML = setupMeetingSheet}
+  
+  // Info button and display
+  const mtgshtInfoBtn = document.getElementById('mtgsetup-info-btn') as HTMLButtonElement
+  if (mtgshtInfoBtn) {
+    mtgshtInfoBtn.addEventListener('click', handleMtgshtInfoBtnClick)
+    mtgshtInfoBtn.addEventListener('blur', handleMtgshtInfoBtnBlur)
+  }
+  const mtgshtInfo = document.getElementById('mtgsetup-info-display')
+  if (mtgshtInfo) {
+    mtgshtInfo.innerHTML = mtgSetupInfo 
+  }
+  
+  // Info button and display for setting individual timer 
+  const mtgshtInfoTimerBtn = document.getElementById('mtgsetup-info-timer-btn') as HTMLButtonElement
+  if (mtgshtInfoTimerBtn) {
+    mtgshtInfoTimerBtn.addEventListener('click', handleMtgshtInfoTimerBtnClick)
+    mtgshtInfoTimerBtn.addEventListener('blur', handleMtgshtInfoTimerBtnBlur)
+  }
+  const mtgshtTimerInfo = document.getElementById('mtgsetup-info-timer-display')
+  if (mtgshtTimerInfo) {
+    mtgshtTimerInfo.innerHTML = mtgSetupInfoTimer 
+  }
+
+  // Info button and display for setting record on 
+  const mtgshtInfoRecordBtn = document.getElementById('mtgsetup-info-record-btn') as HTMLButtonElement
+  if (mtgshtInfoRecordBtn) {
+    mtgshtInfoRecordBtn.addEventListener('click', handleMtgshtInfoRecordBtnClick)
+    mtgshtInfoRecordBtn.addEventListener('blur', handleMtgshtInfoRecordBtnBlur)
+  }
+  const mtgshtRecordInfo = document.getElementById('mtgsetup-info-record-display')
+  if (mtgshtRecordInfo) {
+    mtgshtRecordInfo.innerHTML = mtgSetupInfoRecord 
+  }
+  
+  // Select entity - event listener
   const ent = document.getElementById('mtgsetup-select-entity') as HTMLSelectElement
   if (ent) {  
     ent.addEventListener('change', handleChangedEntitySelection)
   }
+ 
+  // Set individual timer - event listener
   const timer = document.getElementById('timer_type') as HTMLSelectElement
   if (timer) {  
     timer.addEventListener('change', handleChangedTimerType)
@@ -670,6 +779,53 @@ async function handleMeetingSetupDoneButtonClick(this: HTMLElement) {
   setupMeetingSetupListeners()
   setupResetListener()
   setupSpeakingTableSectionChangeListener()
+}
+
+function handleMtgshtInfoBtnClick(this: HTMLButtonElement) {
+  const mtgshtInfo = document.getElementById('mtgsetup-info-display') as HTMLDivElement
+  if (mtgshtInfo) {
+    const vis = window.getComputedStyle(mtgshtInfo).visibility;
+    (vis == 'hidden') ? mtgshtInfo.style.visibility = 'visible' : mtgshtInfo.style.visibility = 'hidden'
+  }
+}
+
+function handleMtgshtInfoBtnBlur() {
+  const mtgshtInfo = document.getElementById('mtgsetup-info-display') as HTMLDivElement
+  if (mtgshtInfo) {
+    mtgshtInfo.style.visibility = 'hidden'
+  }
+}
+
+function handleMtgshtInfoTimerBtnClick(this: HTMLButtonElement) {
+  const mtgshtInfoTimer = document.getElementById('mtgsetup-info-timer-display') as HTMLDivElement
+  if (mtgshtInfoTimer) {
+    const vis = window.getComputedStyle(mtgshtInfoTimer).visibility;
+    (vis == 'hidden') ? mtgshtInfoTimer.style.visibility = 'visible' : mtgshtInfoTimer.style.visibility = 'hidden'
+  }
+}
+
+function handleMtgshtInfoTimerBtnBlur() {
+  const mtgshtInfoTimer = document.getElementById('mtgsetup-info-timer-display') as HTMLDivElement
+  if (mtgshtInfoTimer) {
+    mtgshtInfoTimer.style.visibility = 'hidden'
+  }
+}
+
+
+
+function handleMtgshtInfoRecordBtnClick(this: HTMLButtonElement) {
+  const mtgshtInfoRecord = document.getElementById('mtgsetup-info-record-display') as HTMLDivElement
+  if (mtgshtInfoRecord) {
+    const vis = window.getComputedStyle(mtgshtInfoRecord).visibility;
+    (vis == 'hidden') ? mtgshtInfoRecord.style.visibility = 'visible' : mtgshtInfoRecord.style.visibility = 'hidden'
+  }
+}
+
+function handleMtgshtInfoRecordBtnBlur() {
+  const mtgshtInfoRecord = document.getElementById('mtgsetup-info-record-display') as HTMLDivElement
+  if (mtgshtInfoRecord) {
+    mtgshtInfoRecord.style.visibility = 'hidden'
+  }
 }
 
 /**
